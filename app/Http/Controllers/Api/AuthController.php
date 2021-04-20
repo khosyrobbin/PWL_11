f9c1684647e4bad466e427514ebb3d482e9a34c6
khosyrobbin <khosybin@gmail.com>
2021-04-20 12:12:57 +0700

    add code auth controller

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -10,6 +10,7 @@ use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Hash;
+use App\Http\Controllers\Api;
 
 class AuthController extends Controller
 {

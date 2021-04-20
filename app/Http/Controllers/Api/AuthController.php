62d630de4c230c5ed1ab5b5808a7956b597b4797
khosyrobbin <khosybin@gmail.com>
2021-04-20 12:11:02 +0700

    add code at AuthController

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
 use App\Traits\ApiResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
@@ -25,6 +27,23 @@ class AuthController extends Controller
         return $this->apiSucsess([
             'toke' => $token,
             'token' => 'Bearer',
+            'user' => $user,
+        ]);
+    }
+
+    public function login(LoginRequest $request){
+        $validated = $request->validated();
+
+        if (!Auth::attempt([$validated])) {
+            return $this->apiError('Credential not match', Response::HTTP_UNAUTHORIZED);
+        }
+
+        $user = User::where('email', $validated['email'])->first();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return $this->apiSucsess([
+            'token' => $token,
+            'token_type' => 'Bearer',
             'user' => $user,
         ]);
     }

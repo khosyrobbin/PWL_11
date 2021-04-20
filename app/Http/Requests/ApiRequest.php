8ea15bcfbc65fb594e962fda68edb1242c7d66de
khosyrobbin <khosybin@gmail.com>
2021-04-20 11:48:04 +0700

    create and add code at ApiRequest

--- app/Http/Requests/ApiRequest.php
+++ app/Http/Requests/ApiRequest.php
@@ -2,29 +2,55 @@
 
 namespace App\Http\Requests;
 
+use App\Traits\ApiResponse;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\Response;
 
-class ApiRequest extends FormRequest
+abstract class ApiRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
+    use ApiResponse;
+
+    abstract public function rules();
+
+    protected function failedValidation(Validator $validator)
     {
-        return false;
+        throw new HttpResponseException($this->apiError(
+            $validator->errors(),
+            Response::HTTP_UNPROCESSABLE_ENTITY,
+        ));
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
+    protected function failedAuthorization()
     {
-        return [
-            //
-        ];
+        throw new HttpResponseException($this->apiError(
+            null,
+            Response::HTTP_UNAUTHORIZED
+        ));
     }
+
+
+
+    // /**
+    //  * Determine if the user is authorized to make this request.
+    //  *
+    //  * @return bool
+    //  */
+    // public function authorize()
+    // {
+    //     return false;
+    // }
+
+    // /**
+    //  * Get the validation rules that apply to the request.
+    //  *
+    //  * @return array
+    //  */
+    // public function rules()
+    // {
+    //     return [
+    //         //
+    //     ];
+    // }
 }

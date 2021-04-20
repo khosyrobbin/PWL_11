0d7d396eef7f296116445216a203fb19ef70b31f
khosyrobbin <khosybin@gmail.com>
2021-04-20 13:40:32 +0700

    add code at authController

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
+use App\Http\Requests\LogoutRequest;
 use App\Models\User;
 use App\Traits\ApiResponse;
 use Illuminate\Http\Exceptions\HttpResponseException;
@@ -49,5 +50,17 @@ class AuthController extends Controller
             'token_type' => 'Bearer',
             'user' => $user,
         ]);
+    }
+
+    public function logout(){
+        try{
+            auth()->user()->tokens()->delete();
+            return $this->apiSucsess('Tokens revoked');
+        } catch (\Throwable $e){
+            throw new HttpResponseException($this->apiError(
+                null,
+                Response::HTTP_INTERNAL_SERVER_ERROR,
+            ));
+        }
     }
 }
